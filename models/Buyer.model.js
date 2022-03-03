c18c3a577ec8f7b272d6a21683d9b2d88d2321de
Nicolaes Tollenaar <nico.tollenaar@gmail.com>
2022-03-03 07:12:42 +0100

    saftefy commit before push to own repo

--- models/Buyer.model.js
+++ models/Buyer.model.js
@@ -5,29 +5,29 @@ const buyerSchema = new Schema(
     firstName: {
       type: String,
       required: true,
-      trim: true
+      trim: true,
     },
     lastName: {
       type: String,
       required: true,
-      trim: true
+      trim: true,
     },
     email: {
       type: String,
-      match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.'],
+      match: [/^\S+@\S+\.\S+$/, "Please use a valid email address."],
       required: [true, "Email is required."],
       unique: true,
       lowercase: true,
-      trim: true
+      trim: true,
     },
     passwordHash: {
       type: String,
-      required: [true, "Email is required."]
+      required: [true, "Email is required."],
     },
     affiliation: {
       type: String,
-      trim: true
-    }
+      trim: true,
+    },
   },
   {
     timestamps: true,
